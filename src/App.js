896f860ef487039bdbeb6f25848783dbe4a9f621
Andrea <andreabiondo2000@gmail.com>
2021-11-01 15:59:37 +0100

    modifica header

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="navbar">
+
+      
+
+       <div className="logo">
+        <font color="white">LOGO</font>
+       </div>
+
+       <div className="selectbar">
+       
+         <a href="/App.js">Visualizza repos</a> - <a href="/App.js">Aggiorna repos</a> - <a href="/App.js">Nuovo utente</a>
+       </div>
+      
+      </div>
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div className="container">
+      <div className="precedente">
+        precedente
+      </div>
+      <div className="pagina">
+        pagina
+      </div>
+      <div className="successivo">
+        successivo       
+      </div>
+      </div>
     </div>
   );
 }
