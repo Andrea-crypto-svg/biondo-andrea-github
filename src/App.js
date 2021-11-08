f2d0ae54e0533dda2cc9c4bb8667bc5eeb23faaf
Andrea-crypto-svg <andreabiondo2000@gmail.com>
2021-11-08 12:56:07 +0100

    correzione 0.0.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactTable from "react-table"; 
+import React, { Component } from 'react';
 
 function App() {
   return (
@@ -23,18 +25,56 @@ function App() {
       </header>
 
       <div className="container">
-      <div className="precedente">
-        precedente
+      
       </div>
-      <div className="pagina">
-        pagina
-      </div>
-      <div className="successivo">
-        successivo       
-      </div>
-      </div>
+     
     </div>
   );
+
+  class App extends Component {
+    render(){
+const data = [{  
+  msg: 'Hello',  
+  num: 1  
+  },{  
+  msg: 'Hey',  
+  num: 2  
+  },{  
+  msg: 'Hi',  
+  num: 3   
+  },{  
+  msg: 'Bye',  
+  num: 4  
+  },{  
+  msg: 'Welcome',  
+  num: 5  
+  },{  
+  msg: 'Thanks',  
+  num: 6  
+  }]  
+const columns = [{  
+Header: 'Message',  
+ accessor: 'msg'  
+},{  
+Header: 'Number',  
+accessor: 'num'  
+}]
+      return (  
+    <div>  
+        <ReactTable  
+            data={data}  
+            columns={columns}  
+            defaultPageSize = {2}  
+            pageSizeOptions = {[2,4, 6]}  
+        />  
+    </div>        
+)  
+}  
+  
 }
+}
+    
+ 
+
 
 export default App;
