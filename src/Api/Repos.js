41caa0dc4fdc84ab7aa3d94d044e360368a84538
Andrea-crypto-svg <andreabiondo2000@gmail.com>
2022-02-23 09:34:13 +0100

    commit 11

---
+++ src/Api/Repos.js
@@ -0,0 +1,9 @@
+import * as data from './Mock/Esempio.json';
+
+const listRepos = () => {
+    let repos = data;
+    return repos;
+    
+}
+
+export default listRepos;
---
+++ src/components/totrepos/totrepos.js
@@ -0,0 +1,11 @@
+import './totrepos.css';
+
+const Totrepos = () => {
+    return (
+        <div className='totrepos'>
+            <p>Repository : x</p>
+        </div>
+    )
+}
+
+export default Totrepos;
